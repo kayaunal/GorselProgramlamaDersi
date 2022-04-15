dbe34c12c8d3c46d608879f96f27c09d98a0a162
Kaya ÜNAL <kayaunal@hotmail.com>
2022-04-15 14:23:41 +0300

    Add project files.

---
+++ GorselProgramlamaDersi/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GorselProgramlamaDersi
+{
+    public partial class Form1 : Form
+    {
+        int deger = 10;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int ThisKullanimi(int deger)
+        {
+            this.deger = this.deger * deger;
+            return this.deger;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Sonuç: " + ThisKullanimi(2));
+        }
+    }
+}
