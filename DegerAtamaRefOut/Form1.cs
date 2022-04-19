86d432ae2d853121a0f2ca9b7cd112d253eb27fc
kayaunal <kayaunal@hotmail.com>
2022-04-19 23:16:48 +0300

    Ref ve Out anahtar sözcükleri örneklendi. Params anahtar sözcüğüne kadar gelindi.

---
+++ DegerAtamaRefOut/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DegerAtamaRefOut
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// "deger" metodu
+        /// </summary>
+        /// <param name="deger"></param>
+        /// <returns></returns>
+        public int DegerAtama(int deger)
+        {
+            deger = 100;
+            return deger;
+        }
+        /// <summary>
+        /// REF ile deger metodu
+        /// </summary>
+        /// <param name="refdeger"></param>
+        /// <returns></returns>
+        public int RefDegerAtama(ref int refdeger)
+        {
+            refdeger = 100;
+            return refdeger;
+        }
+        /// <summary>
+        /// OUT ile deger metodu
+        /// </summary>
+        /// <param name="outdeger"></param>
+        /// <returns></returns>
+        public int OutDegerAtama(out int outdeger)
+        {
+            outdeger= 100;
+            return outdeger;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int a = 1;
+            MessageBox.Show("Metot çağrısından önce: " + a.ToString());
+            DegerAtama(a);
+            MessageBox.Show("Metot çağrısından sonra: " + a.ToString());
+            //
+            //Yukarıda değer atama ve sonra çağırmaişlemi gerçekleştirildi.
+            //Dönen değerin aynı olduğuna dikkat!
+            //
+            int b = 1;
+            MessageBox.Show("Ref ile metot çağrısından önce: " + b.ToString());
+            RefDegerAtama(ref b);
+            MessageBox.Show("Ref ile metot çağrısından sonra: " + b.ToString());
+            //
+            //Yukarıda ref ile yapılan tanınlamaların ardından değerin değiştiği gözleniyor.
+            //
+            int c;
+            OutDegerAtama(out c);
+            MessageBox.Show("Out ile metot çağırmadan sonra: " + c.ToString());
+            //
+            //Yukarıda out ile kullanılan değişkene ilk değer atanmadı. 
+        }
+    }
+}
---
+++ ParamsAnahtarSozcugu/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParamsAnahtarSozcugu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Params anahtar sözcüğü, aynı tipte olmak şartıyla metot içerisinde kullanılacak parametre sayısının
+        //bilinmediği bu gibi durumlarda kullanılmaktadır.
+        //Ayrıca dizi ile kullanılmasına rağmen diziyi bilinen dizi yapısından farklı bir şekilde tutar.
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
