552b1100f577cb8128e1d866adff17c26532ef08
kayaunal <77548290+kayaunal@users.noreply.github.com>
2022-04-20 20:44:41 +0300

    Update Form1.cs

--- ParamsAnahtarSozcugu/Form1.cs
+++ ParamsAnahtarSozcugu/Form1.cs
@@ -19,9 +19,22 @@ namespace ParamsAnahtarSozcugu
         //Params anahtar sözcüğü, aynı tipte olmak şartıyla metot içerisinde kullanılacak parametre sayısının
         //bilinmediği bu gibi durumlarda kullanılmaktadır.
         //Ayrıca dizi ile kullanılmasına rağmen diziyi bilinen dizi yapısından farklı bir şekilde tutar.
+        
+        public int ParamsEnBuyukSayiyiBul(params int[] sayilar) 
+        { 
+        int enBuyukSayi = -1; 
+        if (sayilar.Length > 0) 
+        enBuyukSayi = sayilar[0]; 
+        for (int i = 1; i < sayilar.Length; i++) 
+        { 
+        if (enBuyukSayi < sayilar[i]) enBuyukSayi = sayilar[i]; 
+        } 
+        return enBuyukSayi; 
+        }
+        
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            MessageBox.Show("En büyük sayı: "+ ParamsEnBuyukSayiyiBul(7,10,3,4,9));
         }
     }
 }
