1e17fbffd2b8a4a6962ecee50f9cef5d133f568f
kayaunal <kayaunal@hotmail.com>
2022-05-04 22:43:58 +0300

    String sınıfındaki çeşitli metodlara ilişkin denemeler yapıldı.

--- StringSinifi/Form1.Designer.cs
+++ StringSinifi/Form1.Designer.cs
@@ -29,46 +29,222 @@
         private void InitializeComponent()
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button9 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.button11 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(81, 77);
+            this.textBox1.Location = new System.Drawing.Point(15, 68);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(195, 126);
+            this.textBox1.Size = new System.Drawing.Size(129, 117);
             this.textBox1.TabIndex = 0;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(362, 77);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 228);
-            this.textBox2.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(81, 282);
+            this.button1.Location = new System.Drawing.Point(15, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(129, 23);
+            this.button1.Size = new System.Drawing.Size(120, 23);
             this.button1.TabIndex = 2;
             this.button1.Text = "CopyTo Metodu";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(150, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 173);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(15, 230);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Clone Method";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(15, 259);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Insert Method";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(15, 288);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(120, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Replace Method";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(15, 317);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(120, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Substring Method";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(686, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(102, 20);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(686, 38);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(102, 23);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "Compare Method";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(341, 12);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(142, 57);
+            this.textBox3.TabIndex = 10;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(489, 12);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(142, 57);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(362, 75);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(100, 23);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "IndexOf Method";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(489, 128);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(96, 23);
+            this.button8.TabIndex = 12;
+            this.button8.Text = "Lenght";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(341, 128);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(142, 52);
+            this.textBox5.TabIndex = 10;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(489, 157);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(96, 23);
+            this.button9.TabIndex = 12;
+            this.button9.Text = "Count()";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(341, 216);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(142, 57);
+            this.textBox6.TabIndex = 10;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(489, 216);
+            this.textBox7.Multiline = true;
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(142, 57);
+            this.textBox7.TabIndex = 10;
+            this.textBox7.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(362, 279);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(100, 23);
+            this.button11.TabIndex = 13;
+            this.button11.Text = "Arama Butonu";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button6);
             this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -77,8 +253,23 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button button11;
     }
 }
 
--- StringSinifi/Form1.cs
+++ StringSinifi/Form1.cs
@@ -19,7 +19,135 @@ namespace StringSinifi
 
         private void button1_Click(object sender, EventArgs e)
         {
+            string ifade = textBox1.Text;
+            char[] dizi = new char[20];
+            ifade.CopyTo(4, dizi, 0, 7);
+            foreach (char eleman in dizi)
+            {
+                listBox1.Items.Add(eleman);
+            }
+        }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string ifade = "Açıköğretim Fakültesi";
+            string klon = (string)ifade.Clone();
+            MessageBox.Show(klon);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string cumle = "bana yardım eder misin?";
+            string ekCumle = cumle.Insert(0, "Lütfen ");
+            MessageBox.Show(ekCumle);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string ifade = "Açıköğretim Fakültesi";
+            string yeniIfade = ifade.Replace("Açıköğretim", "İkinci Üniversite");
+            MessageBox.Show(yeniIfade);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string cumle = "Açıköğretim Fakültesi";
+            string sonuc = cumle.Substring(cumle.Length - 9);
+            MessageBox.Show(sonuc);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string veriTabanindakiParola = "Giris.123";
+            string parolaCheck = textBox2.Text;
+            int sonuc = string.Compare(veriTabanindakiParola, parolaCheck);
+            if (sonuc == 0)
+            {
+                MessageBox.Show("Parola doğru");
+            }
+            else MessageBox.Show("Parola hatalı!");
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string ifade = textBox3.Text;
+            string arananKelime = textBox4.Text;
+            int sonuc = ifade.IndexOf(arananKelime);
+            if (sonuc != -1)
+            {
+                MessageBox.Show("Aranan kelimenin bulunduğu index: " + sonuc.ToString());
+            }
+            else
+            {
+                MessageBox.Show("Aranan kelime bulunamamaktadır.");
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            string dataFromTextBox5 = textBox5.Text;
+            int boyut = dataFromTextBox5.Length;
+            MessageBox.Show("Yazılan ifadenin 'lenght' ile hesaplanan karakter sayısı: " + boyut);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            string dataFromTextBox5 = textBox5.Text;
+            int boyut = dataFromTextBox5.Count();
+            MessageBox.Show("Yazılan ifadenin 'count() ile hesaplanan' karakter sayısı" + boyut);
+        }
+        //Aşağıdaki bölüme daha sonra şöyle bir uygulama geliştirilecek:
+        //
+        //Kullanıcıdan alınan bir metin içerisinde yine kullanıcıdan
+        //alınan bir kelimeyi arayan, aranan kelimenin bu metinde
+        //kaç kez geçtiğini bulan ve tüm bu kelimeyi yine kullanıcıdan
+        //alınan ikinci bir kelime ile değiştiren uygulama
+        private void button11_Click(object sender, EventArgs e)
+        {
+            string dataFromTextBox6 = textBox6.Text;
+            string dataFromTextBox7 = textBox7.Text;
+            bool sonuc = dataFromTextBox6.Contains(dataFromTextBox7);
+            int sayi = 0;
+            if (sonuc == true)
+            {
+                for (int i = 0; i < dataFromTextBox6.Length; i++)
+                {
+                    i++;
+                }
+            MessageBox.Show("Metin içerisinde " + sayi + " adet " + dataFromTextBox7 + " bulunmaktadır.");
+            }
+            else
+            {
+                MessageBox.Show("Sonuç bulunamadı");
+            }
         }
     }
 }
